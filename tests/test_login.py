4a74be4cc3e76e2d0f430d9e3e55f96aea259a05
Youngrok Pak <pak.youngrok@gmail.com>
2024-12-04 15:39:00 +0900

    More commonly used name

--- tests/test_login.py
+++ tests/test_login.py
@@ -27,10 +27,10 @@ class TestHometaxLogin(unittest.TestCase):
         scraper = HometaxScraper()
         scraper.login_with_cert(testdata.CORP_CERT, testdata.CORP_PASSWORD)
 
-        self.assertEqual(홈택스사용자구분코드.법인사업자, scraper.user_info.사용자구분)
+        self.assertEqual(홈택스사용자구분코드.법인, scraper.user_info.사용자구분)
         self.assertIn('주식회사', scraper.user_info.납세자명)
 
-        self.assertEqual(scraper.selected_trader.사업자구분, 홈택스사용자구분코드.법인사업자)
+        self.assertEqual(scraper.selected_trader.사업자구분, 홈택스사용자구분코드.법인)
         self.assertEqual(10, len(scraper.selected_trader.납세자번호))
         self.assertIsNotNone(scraper.selected_trader.사업장소재지)
         self.assertIsNotNone(scraper.selected_trader.사업장전화번호)
